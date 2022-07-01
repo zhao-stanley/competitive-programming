8b945c86873801d00b417619aeb997161b64dd0f
zhao-stanley <contact@stanleyweb.dev>
2022-06-30 20:24:54 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int numLines;
+
+    cin >> numLines; //Take in integer representing number of lines
+    for (int i = 0; i < numLines; i++){ //loop through all the lines
+        //do whatever
+    }
+}
+
+//barebones file that I use
