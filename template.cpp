9acb84109bdc13baff056c5b8a51740ce71df781
Stanley Zhao <101726102+zhao-stanley@users.noreply.github.com>
2022-07-12 10:43:59 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-//#include<bits/stdc++.h> --> set and a bunch of other stuff idk
-//#include <string> --> string stuff
-//#include <math.h> --> math
+//#include<bits/stdc++.h>
+//#include <string> 
+//#include <math.h>
 using namespace std;
 
 
@@ -9,10 +9,8 @@ int main()
 {
     int numLines;
 
-    cin >> numLines; //Take in integer representing number of lines
-    for (int i = 0; i < numLines; i++){ //loop through all the lines
-        //do whatever
+    cin >> numLines; 
+    for (int i = 0; i < numLines; i++){
+        
     }
 }
-
-//barebones file that I use
