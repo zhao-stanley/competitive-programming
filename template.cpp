b0357ba2df1337591c563b951ab3a6df6d378897
zhao-stanley <contact@stanleyweb.dev>
2022-07-12 14:44:16 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,4 +1,6 @@
+using namespace std;
 #include <iostream>
+#include <vector>
 //#include<bits/stdc++.h>
 //#include <string> 
 //#include <math.h>
@@ -10,19 +12,20 @@ using vb = vector<bool>;
 using pii = pair<int, int>;
 
 #define pb push_back
-#define sz(a) ((int) ((a).size()))
+#define nl "\n"
+#define sz(a) static_cast<int>((a).size())
 #define all(x) begin(x), end(x)
-using namespace std;
-
 
 int main()
 {
     //Fast I/O
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     int t;
     cin >> t; 
-    while(t--){
 
+    while(t--){
+        
     }
 }
