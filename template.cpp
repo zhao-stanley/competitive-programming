432523dfaab844a3096354fb631bee899dfefb95
zhao-stanley <contact@stanleyweb.dev>
2022-07-12 14:13:44 -0400

    New boilerplate

--- template.cpp
+++ template.cpp
@@ -2,15 +2,27 @@
 //#include<bits/stdc++.h>
 //#include <string> 
 //#include <math.h>
+//#include <algorithm>
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+
+#define pb push_back
+#define sz(a) ((int) ((a).size()))
+#define all(x) begin(x), end(x)
 using namespace std;
 
 
 int main()
 {
-    int numLines;
+    //Fast I/O
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t; 
+    while(t--){
 
-    cin >> numLines; 
-    for (int i = 0; i < numLines; i++){
-        
     }
 }
